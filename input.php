373a06a3c844f0f68a5944f210465a21f0e4fe69
TrieuLE0801 <leviettrieu369@gmail.com>
2019-05-10 12:07:31 +0700

    Modify css

--- input.php
+++ input.php
@@ -1,15 +1,59 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<meta name="input" content="width=device-width, initial-scale=1">
+		<style type="text/css">
+			* {
+ 				 box-sizing: border-box;
+			}
+			/*class containers*/
+			.container {
+ 				 padding: 16px;
+			}
+			/*full-width input field*/
+			input[type=text], input[type=password] {
+				width: 100%;
+				padding: 15px;
+				margin: 5px 0 22px 0;
+				display: inline-block;
+				border: none;
+				background: #f1f1f1f1;
+			}
+			input[type=text]:focus, input[type=password]:focus{
+				background-color: #ddd;
+				outline: none;
+			}
+			input[type=number], input[type=number] {
+				width: 100%;
+				padding: 15px;
+				margin: 5px 0 22px 0;
+				display: inline-block;
+				border: none;
+				background: #f1f1f1f1;
+			}
+			/* Set a style for the submit/register button */
+			.submit {
+			  background-color: #07A7F9;
+			  color: white;
+			  padding: 16px 20px;
+			  margin: 8px 0;
+			  border: none;
+			  cursor: pointer;
+			  width: 100%;
+			  opacity: 0.9;
+			}
+
+		</style>
 	</head>
 	<body>
 		<h2 style="text-align: center;">INSERT PRODUCT INFORMATION</h2>
-		<form action="insert.php" method="POST">
+		<form action="input.php" method="POST">
+			<div class="container">
 			Product link: <input type="text" name="link" /><br><br>
 			Quantity: <input type="number" name="quantity" /><br><br>
 			Code: <input type="text" name="code" /><br><br>
 			<input class="submit" type ="submit" name="submit" value="Save"/>
+		</div>
 		</form>
 		<?php
 			$servername = "localhost";
@@ -33,10 +77,14 @@
 				$code = $_POST['code'] ? $_POST['code'] : '';
 
 				if ($link && $quantity){
-					$sql = "INSERT INTO items (product, quality, code)
-					VALUES ('$link', $quantity, '$code')";
-					$conn->query($sql);
-		            echo "Insert information successfully.";
+					if ($quantity < 0){
+					echo "Please enter the greater-than-0 number.";
+				}else{
+						$sql = "INSERT INTO items (product, quality, code)
+						VALUES ('$link', $quantity, '$code')";
+						$conn->query($sql);
+			            echo "Insert information successfully.";
+		        	}
 				}else{
 		  			echo "Required fields are missing.";
 				}
