a4d3ef545c816aa435c275d523528f5872d3e1ae
jokerjackpot921731 <34645799+jokerjackpot921731@users.noreply.github.com>
2019-05-15 22:56:12 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,37 +1,60 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
 <head>
 <style>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 #myInput {
-  width: 100%;
-  font-size: 16px;
-  padding: 12px 20px 12px 3px;
-  border: 1px solid #ddd;
-  margin-bottom: 12px;
+    width: 100%;
+    font-size: 16px;
+    padding: 12px 20px 12px 3px;
+    border: 1px solid #ddd;
+    margin-bottom: 12px;
 }
 
-#myTable {
-  border-collapse: collapse;
-  width: 100%;
-  border: 1px solid #ddd;
-  font-size: 18px;
+#lightTable {
+    border-collapse: collapse;
+    width: 100%;
+    border: 2px solid #ddd;
+    font-size: 18px;
 }
 
-#myTable th, #myTable td {
-  text-align: left;
-  padding: 12px;
+#lightTable th, #lightTable td {
+    text-align: left;
+    padding: 12px;
+    border: 1px solid #ddd;
 }
 
-#myTable tr {
-  border-bottom: 1px solid #ddd;
+#lightTable tr {
+    border-bottom: 1px solid #ddd;
 }
 
-#myTable tr.header, #myTable tr:hover {
-  background-color: #f1f1f1;
+#lightTable tr.header, #lightTable tr:hover {
+    background-color: #f1f1f1;
+}
+
+#darkTable {
+    border-collapse: collapse;
+    background-color: #8a8a5c;
+    width: 100%;
+    border: 2px solid #ddd;
+    font-size: 18px;
+}
+
+#darkTable th, #darkTable td {
+    text-align: left;
+    padding: 12px;
+    border: 1px solid #ddd;
+}
+
+#darkTable tr {
+    border-bottom: 1px solid #ddd;
+}
+
+#darkTable tr.header, #darkTable tr:hover {
+    background-color: #f1f1f1;
 }
 
 #search{
@@ -56,6 +79,12 @@
         <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
     </div>
 
+    <form action="" method="post" style="padding-bottom: 20px">
+    <input type="radio" name="radio" value="dark">Dark background
+    <input type="radio" name="radio" value="light">Light background
+    <input type="submit" name="submit" value="Select background font" />
+    </form>
+
     <?php
         $servername = "localhost";
         $dbname = "test";
@@ -66,47 +95,127 @@
         $use = "USE test";
         $conn->query($use);
 
+        // set cookie
+        $cookieName = 'backgroundCookie';
+        $cookieValue = 'light';
+        if (isset($_POST['submit'])) {
+            if(isset($_POST['radio']))
+            {
+                $cookieName = 'backgroundCookie';
+                $cookieValue = $_POST['radio'];
+            }
+        }
+        setcookie($cookieName, $cookieValue, time() + (3600), "/");
+        if ($_COOKIE[$cookieName] == 'dark') {
+            //query all database
+            $query = "SELECT * FROM items"; 
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+                echo "<table id=\"darkTable\">";
+                echo "<tr id=\"FirstRow\">"; 
+                echo "<th>Id</th>"; 
+                echo "<th>Name Product</th>"; 
+                echo "<th>Link</th>"; 
+                echo "<th>Quality</th>"; 
+                echo "<th>Code</th>"; 
+                echo "<th>Status</th>"; 
+                echo "<th>TimeDeli</th>"; 
+                echo "</tr>";
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>"; 
+                    // foreach ($row as $value) {
+                    //     echo "<td>".$value."</td>";
+                    // }
+                    echo "<td>".$row['id']."</td>"; 
+                    echo "<td>".$row['name']."</td>"; 
+                    echo "<td><a href=\"".$row['product']."\">".$row['product']."</a></td>"; 
+                    echo "<td>".$row['quality']."</td>"; 
+                    // echo split(",", $row['code'] [, int $limit = -1 ]);
+                    echo "<td>".$row['code']."</td>"; 
+                    echo "<td>".$row['status']."</td>"; 
+                    echo "<td>".$row['timeDeli']."</td>"; 
+                    echo "</tr>"; 
+                    // $arrayName = split(",", $row['code']);
+                    // echo $arrayName;
+                }
+                echo "</table>";
+            } else {
+                echo "Table has been deleted";
+            }
+        } else {
+            //query all database
+            $query = "SELECT * FROM items"; 
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+                echo "<table id=\"lightTable\">";
+                echo "<tr id=\"FirstRow\">"; 
+                echo "<th>Id</th>"; 
+                echo "<th>Name Product</th>"; 
+                echo "<th>Link</th>"; 
+                echo "<th>Quality</th>"; 
+                echo "<th>Code</th>"; 
+                echo "<th>Status</th>"; 
+                echo "<th>TimeDeli</th>"; 
+                echo "</tr>";
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>"; 
+                    // foreach ($row as $value) {
+                    //     echo "<td>".$value."</td>";
+                    // }
+                    echo "<td>".$row['id']."</td>"; 
+                    echo "<td>".$row['name']."</td>"; 
+                    echo "<td><a href=\"".$row['product']."\">".$row['product']."</a></td>"; 
+                    echo "<td>".$row['quality']."</td>"; 
+                    // echo split(",", $row['code'] [, int $limit = -1 ]);
+                    echo "<td>".$row['code']."</td>"; 
+                    if ($row['status'] == 'Delivered') {
+                        echo "<td style=\"background-color: #00cc44\">".$row['status']."</td>";
+                    } else {
+                        if ($row['status'] == 'Return check') {
+                            echo "<td style=\"background-color: #ff1a1a\">".$row['status']."</td>";
+                        } else {
+                            echo "<td>".$row['status']."</td>";
+                        }
+                        
+                    }
+                    echo "<td>".$row['timeDeli']."</td>"; 
+                    echo "</tr>"; 
+                    // $arrayName = split(",", $row['code']);
+                    // echo $arrayName;
+                }
+                echo "</table>";
+            } else {
+                echo "Table has been deleted";
+            }
+        }
+
+        // $drop = "DROP TABLE items";
+        // $conn->query($drop);
+
+        // $sql = "CREATE TABLE items (
+        // id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        // name TEXT NOT NULL,
+        // product TEXT NOT NULL,
+        // quality TEXT NOT NULL,
+        // code TEXT,
+        // status TEXT,
+        // timeDeli TEXT,
+        // isDelivered BOOLEAN DEFAULT '0',
+        // haveCode BOOLEAN DEFAULT '0'
+        // )";
+        // $conn->query($sql);
+
         // //sql insert a document
         // $sql1 = "INSERT INTO Items (name, product, quality, code, status, timeDeli)
-        // VALUES ('Su tu', 'https://vi.wikipedia.org/wiki/S%C6%B0_t%E1%BB%AD', '3', '675456456', 'transfering', '1 - Deli')";
+        // VALUES ('Su tu', 'https://vi.wikipedia.org/wiki/S%C6%B0_t%E1%BB%AD', '3', '485921176180,1LS7261053992577-1', '', '')";
         // $sql2 = "INSERT INTO Items (name, product, quality, code, status, timeDeli)
-        // VALUES ('Heo', 'https://vi.wikipedia.org/wiki/L%E1%BB%A3n_nh%C3%A0', '3', '678675645', 'transfering', '2 - Deli')";
+        // VALUES ('Heo', 'https://vi.wikipedia.org/wiki/L%E1%BB%A3n_nh%C3%A0', '3', '464131010998', '', '')";
         // $conn->query($sql1);
         // $conn->query($sql2);
 
-        //query all database
-        $query = "SELECT * FROM items"; 
-        $result = $conn->query($query);
-        if ($result->num_rows > 0) {
-            echo "<table id=\"myTable\">";
-            echo "<tr id=\"FirstRow\">"; 
-            echo "<th>Id</th>"; 
-            echo "<th>Name Product</th>"; 
-            echo "<th>Link</th>"; 
-            echo "<th>Quality</th>"; 
-            echo "<th>Code</th>"; 
-            echo "<th>Status</th>"; 
-            echo "<th>TimeDeli</th>"; 
-            echo "</tr>";
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "<tr>"; 
-                // foreach ($row as $value) {
-                //     echo "<td>".$value."</td>";
-                // }
-                echo "<td>".$row['id']."</td>"; 
-                echo "<td>".$row['name']."</td>"; 
-                echo "<td><a href=\"".$row['product']."\">".$row['product']."</a></td>"; 
-                echo "<td>".$row['quality']."</td>"; 
-                echo "<td>".$row['code']."</td>"; 
-                echo "<td>".$row['status']."</td>"; 
-                echo "<td>".$row['timeDeli']."</td>"; 
-                echo "</tr>"; 
-            }
-            echo "</table>";
-        } else {
-            echo "Table has been deleted";
-        }
+        
 
         $conn->close()
     ?>
@@ -115,7 +224,7 @@
       var input, filter, table, rows, td, i, txtValue;
       input = document.getElementById("myInput");
       filter = input.value.toUpperCase();
-      table = document.getElementById("myTable");
+      table = document.getElementById("lightTable");
       rows = table.getElementsByTagName("tr");
       for (i = 0; i < rows.length; i++) {
         td = rows[i].getElementsByTagName("td")[1];
